6a3f50db69805399f7c65761ccba84da28301f11
davlloyd <davidlloyd0@gmail.com>
2021-10-17 21:04:28 +1100

    Made all config files created programattically

--- tap_managefiles.sh
+++ tap_managefiles.sh
@@ -37,11 +37,17 @@ else
 fi
 shopt -u nocasematch
 
+####
+log "Creating configuration file: app-accelerator-values.yaml"
+
 cat > "app-accelerator-values.yaml" <<EOF
 server:
   service_type: "$SERVICE_TYPE"
   watched_namespace: "$DEV_NAMESPACE"
 EOF
+
+####
+log "Creating configuration file: app-live-view-values.yaml"
 
 cat > "app-live-view-values.yaml" <<EOF
 ---
@@ -49,10 +55,16 @@ connector_namespaces: [$DEV_NAMESPACE]
 server_namespace: app-live-view
 EOF
 
+####
+log "Creating configuration file: cnr-values.yaml"
+
 cat > "cnr-values.yaml" <<EOF
 ---
 $CNR_PROVIDER
 EOF
+
+####
+log "Creating configuration file: default-supply-chain-values.yaml"
 
 cat > "default-supply-chain-values.yaml" <<EOF
 ---
@@ -62,6 +74,8 @@ registry:
 service_account: service-account
 EOF
 
+####
+log "Creating configuration file: tbs-values.yaml"
 
 cat > "tbs-values.yaml" <<EOF
 ---
@@ -72,6 +86,9 @@ tanzunet_username: $PIVNET_ACCOUNT
 tanzunet_password: $PIVNET_PASSWORD
 EOF
 
+####
+log "Creating configuration file: cnr-custom-domain.yaml"
+
 cat > "cnr-custom-domain.yaml" <<EOF
 apiVersion: v1
 kind: ConfigMap
@@ -80,4 +97,158 @@ metadata:
   namespace: knative-serving
 data:
   $CUSTOM_DOMAIN: ""
+EOF
+
+####
+log "Creating configuration file: scst-sign-values.yaml"
+
+cat > "scst-sign-values.yaml" <<EOF
+---
+warn_on_unmatched: true
+EOF
+
+####
+log "Creating configuration file: app-live-view-values.yaml"
+
+cat > "scst-store-values.yaml" <<EOF
+api_host: "localhost"
+api_port: 9443
+app_service_type: "LoadBalancer"
+auth_proxy_host: "0.0.0.0"
+auth_proxy_port: 8443
+db_host: "metadata-store-db"
+db_name: "metadata-store"
+db_password: "password"
+db_port: "5432"
+db_sslmode: "verify-full"
+db_user: "metadata-store-user"
+storageClassName: "manual"
+use_cert_manager: "true"
+EOF
+
+####
+log "Creating configuration file: imagepolicy-cosign.yaml"
+
+cat > "imagepolicy-cosign.yaml" <<EOF
+---
+
+apiVersion: signing.run.tanzu.vmware.com/v1alpha1
+kind: ClusterImagePolicy
+metadata:
+ name: image-policy
+spec:
+ verification:
+   exclude:
+     resources:
+       namespaces:
+       - kube-system
+   keys:
+   - name: cosign-key
+     publicKey: |
+       -----BEGIN PUBLIC KEY-----
+       MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEhyQCx0E9wQWSFI9ULGwy3BuRklnt
+       IqozONbbdbqz11hlRJy9c7SG+hdcFl9jE9uE/dwtuwU2MqU9T/cN0YkWww==
+       -----END PUBLIC KEY-----
+   images:
+   - namePattern: gcr.io/projectsigstore/cosign*
+     keys:
+     - name: cosign-key
+EOF
+
+####
+log "Creating configuration file: metastore-rbac.yaml"
+cat > "metastore-rbac.yaml" <<EOF
+---
+apiVersion: rbac.authorization.k8s.io/v1
+kind: Role
+metadata:
+  name: metadata-store-read-write
+  namespace: metadata-store
+rules:
+- resources: ["all"]
+  verbs: ["get", "create", "update"]
+  apiGroups: [ "metadata-store/v1" ]
+
+---
+apiVersion: rbac.authorization.k8s.io/v1
+kind: RoleBinding
+metadata:
+  name: metadata-store-read-write
+  namespace: metadata-store
+roleRef:
+  apiGroup: rbac.authorization.k8s.io
+  kind: Role
+  name: metadata-store-read-write
+subjects:
+- kind: ServiceAccount
+  name: metadata-store-read-write-client
+  namespace: metadata-store
+
+---
+apiVersion: v1
+kind: ServiceAccount
+metadata:
+  name: metadata-store-read-write-client
+  namespace: metadata-store
+automountServiceAccountToken: false
+EOF
+
+####
+log "Creating configuration file: dev-namespace-enable.yaml"
+cat > "dev-namespace-enable.yaml" <<EOF
+apiVersion: v1
+kind: Secret
+metadata:
+  name: tap-registry
+  annotations:
+    secretgen.carvel.dev/image-pull-secret: ""
+type: kubernetes.io/dockerconfigjson
+data:
+  .dockerconfigjson: e30K
+
+---
+apiVersion: v1
+kind: ServiceAccount
+metadata:
+  name: service-account # use value from "Install Default Supply Chain"
+secrets:
+  - name: registry-credentials
+imagePullSecrets:
+  - name: registry-credentials
+  - name: tap-registry
+
+---
+apiVersion: rbac.authorization.k8s.io/v1
+kind: Role
+metadata:
+  name: kapp-permissions
+  annotations:
+    kapp.k14s.io/change-group: "role"
+rules:
+  - apiGroups:
+      - servicebinding.io
+    resources: ['servicebindings']
+    verbs: ['*']
+  - apiGroups:
+      - serving.knative.dev
+    resources: ['services']
+    verbs: ['*']
+  - apiGroups: [""]
+    resources: ['configmaps']
+    verbs: ['get', 'watch', 'list', 'create', 'update', 'patch', 'delete']
+
+---
+apiVersion: rbac.authorization.k8s.io/v1
+kind: RoleBinding
+metadata:
+  name: kapp-permissions
+  annotations:
+    kapp.k14s.io/change-rule: "upsert after upserting role"
+roleRef:
+  apiGroup: rbac.authorization.k8s.io
+  kind: Role
+  name: kapp-permissions
+subjects:
+  - kind: ServiceAccount
+    name: service-account # use value from "Install Default Supply Chain"
 EOF
